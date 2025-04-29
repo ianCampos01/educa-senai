005e86444ff0a0be25fceb4848f0dce0b7c46983
andrielly-lima <andriellyfranlima.2007@gmail.com>
2025-04-29 10:58:30 -0300

    modificações feature/#14

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+document.getElementById("add").addEventListener("click", function(e){
+    //pegando valores do input Html
+    const infoRotina = document.getElementById("rotina").value;
+
+    
+});

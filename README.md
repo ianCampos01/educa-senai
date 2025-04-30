65195e985ed1956d5e5b9a6e4fb188e7b58e67a1
ianCampos01 <iancampos09008@gmail.com>
2025-04-29 21:28:29 -0300

    conclusão feature/#14

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# educa-senai
+# Sua Rotina - Gerenciador de Tarefas Simples
+- Este projeto é um simples gerenciador de tarefas que permite adicionar itens a duas listas: uma ordenada e outra não ordenada. Ideal para organizar sua rotina diária.
+
+# Funcionalidades:
+- Adicionar tarefas a uma lista ordenada e uma lista não ordenada.
+- Validação de entrada: impede a adição de tarefas em branco.
+- Interface intuitiva e fácil de usar.
+## Como usar:
+- Clone o repositório: git clone <link_do_repositorio>
+- Abra o arquivo index.html em seu navegador.
+- Digite sua tarefa no campo de texto e clique em 'Adicionar'. A tarefa será adicionada a ambas as listas.
+- Pressione Enter no campo de texto para adicionar a tarefa rapidamente.
+- Tecnologias Utilizadas:
+- HTML
+- CSS
+- JavaScript
+## Contribuições:
+- Contribuições são bem-vindas! Sinta-se à vontade para abrir issues ou enviar pull requests.
+
+# Autor:
+    Ian Campos/ianCampos01
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,42 @@
-document.getElementById("add").addEventListener("click", function(e){
-    //pegando valores do input Html
-    const infoRotina = document.getElementById("rotina").value;
+const buttonConfirm = document.getElementById("add"); // pegando id do botão no html
+const RoutineInformation = document.getElementById("rotina"); // pegando valores do input Html
 
-    
+function elementsForList(valueForInput){
+    const listNotOrdened = document.getElementById("listNotOrdened");// pegando id da lista não ordenada
+    const listOrdened = document.getElementById("listOrdened"); // pegando id da lista ordenada
+
+    let createElementUl = document.createElement("li");
+    let createElementOl = document.createElement("li");
+
+    createElementUl.textContent = `${valueForInput}`;
+    createElementOl.textContent = valueForInput;
+
+    listOrdened.appendChild(createElementUl);
+    listNotOrdened.appendChild(createElementOl);
+
+}
+
+function toSend(){
+    const valueForInput = RoutineInformation.value.trim();
+    let fieldError = document.getElementById("fieldError");
+
+    if(valueForInput === ""){
+        RoutineInformation.style.border = "2px solid red";
+        return fieldError.innerHTML = "Campo Vazio!";
+    };
+
+    fieldError.innerHTML = "";
+
+    RoutineInformation.style.border = "";
+
+    elementsForList(valueForInput);
+    RoutineInformation.value = '';
+}
+
+buttonConfirm.addEventListener("click", toSend)
+
+RoutineInformation.addEventListener("keydown", function(e){
+    if(event.key === "Enter"){
+        toSend();
+    }
 });
